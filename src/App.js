4fbc2c0e0f026540d6105db3879d3210003d077f
rohitnbt <rohitsinghnbt@gmail.com>
2023-12-26 21:48:16 +0530

    add tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-7xl text-blue'>Hello World</h1>
+      <h1 className='text-20xl'>Hello World</h1>
     </div>
   );
 }
